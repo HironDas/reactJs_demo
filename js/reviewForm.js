42a02f746ddfde3753c7bbef3a86df26d12c3562
Hiron Das <hcdas.09@gmail.com>
2016-04-20 02:59:26 +0600

    a little bit remaining of lecture 2

---
+++ js/reviewForm.js
@@ -0,0 +1,10 @@
+var courseList = [
+	{courseName: "Master Reactjs"},
+	{courseName: "Master Angularjs"},
+	{courseName: "Master JavaScript and jQuery"},
+	{courseName: "Browaer Developer Tools"},
+	{courseName: "Master Knockoutjs"},
+	{courseName: "Advance jQuery for Designers & Developer"},
+	{courseName: "Amazom EC2 LAMP"},
+	{courseName: "Node js"}
+];
